f434bc3d45d46552da54e32ce70932bc21e46d39
cjmarling <cjmarling@gmail.com>
2021-07-03 07:26:11 -0700

    initial network release

--- main.tf
+++ main.tf
@@ -13,6 +13,19 @@ locals {
 }
 
 # Network Configuration
+module "aws_network" {
+  source = "github.com/cjmarling/module-aws-network"
+
+  env_name              = local.env_name
+  vpc_name              = "msur_VPC"
+  cluster_name          = local.k8s_cluster_name
+  aws_region            = local.aws_region
+  main_vpc_cidr         = "10.10.0.0/16"
+  public_subnet_a_cidr  = "10.10.0.0/18"
+  public_subnet_b_cidr  = "10.10.64.0/18"
+  private_subnet_a_cidr = "10.10.128.0/18"
+  private_subnet_b_cidr = "10.10.192.0/18"
+}
 
 # EKS Configuration
 
